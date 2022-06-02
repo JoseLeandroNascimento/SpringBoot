ef2a85b3dc76ad855ea39e38a3e2ac654df173a4
j.leandro1998silva@gmail.com <j.leandro1998silva@gmail.com>
2022-06-02 11:08:38 -0500

    criação de conexao e de tabelas do banco

--- app/src/main/java/com/projeto/app/AppApplication.java
+++ app/src/main/java/com/projeto/app/AppApplication.java
@@ -1,23 +1,10 @@
 package com.projeto.app;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class AppApplication {
-
-	@Value("${spring.application.name}")
-	private String applicationName;
-
-	@GetMapping("/hello")
-	public String hello(){
-
-		return applicationName;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
